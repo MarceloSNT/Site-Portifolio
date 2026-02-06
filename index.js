0b59c773da707141afd4d56a8e5be7300ab34bc5
Marcelo <marcelodossantosmachado05@gmail.com>
2026-02-06 19:53:07 -0300

    Feat: Add translate from Portuguese Brazil to English EUA.

---
+++ index.js
@@ -0,0 +1,135 @@
+const { title } = require("process");
+
+function traduzirEn() {
+  aboutme = document.getElementById("about-me-text");
+  homeText = document.getElementById("home-text");
+  homeName = document.getElementById("home-name");
+
+  project1 = document.getElementById("project-1");
+  project2 = document.getElementById("project-2");
+  project3 = document.getElementById("project-3");
+  project4 = document.getElementById("project-4");
+
+  headerAboutMe = document.getElementById("header-about-me");
+  headerProjects = document.getElementById("header-projects");
+  headerTools = document.getElementById("header-tools");
+  headerContact = document.getElementById("header-contact");
+
+  titleAboutMe = document.getElementById("title-about-me");
+  titleProjects = document.getElementById("title-projects");
+  titleTools = document.getElementById("title-tools");
+  titleContact = document.getElementById("title-contacts");
+
+  subtitleBackend = document.getElementById("subtitle-backend");
+  subtitleFrontend = document.getElementById("subtitle-frontend");
+  subtitleDatabase = document.getElementById("subtitle-database");
+  subtitleTools = document.getElementById("subtitle-tools");
+
+  phraseContact = document.getElementById("phrase-contact");
+  footerRights = document.getElementById("footer-rights");
+
+  aboutme.innerText =
+    "I am a Systems Analysis and Development student at Unisul, with a great passion for software development and a constant interest in evolving technically. Throughout my education, I have been actively involved in various projects that have allowed me to apply my knowledge and skills in real-world scenarios. I am eager to continue learning and growing as a developer, always seeking new challenges and opportunities to expand my expertise.";
+
+  homeText.innerText =
+    "Welcome to my portfolio! Here I share projects and solutions focused on Fullstack development. Feel free to explore my work and get to know my journey.";
+
+  homeName.innerText = "I'm Marcelo";
+
+  project1.innerText =
+    "I participated as a front-end developer in the academic project Loja E-commerce Vivalis, my first practical experience in developing an e-commerce platform. I was responsible for implementing the user interface and ensuring a seamless user experience. This project allowed me to apply my knowledge of front-end technologies and collaborate effectively with the team to deliver a functional and visually appealing online store.";
+
+  project2.innerText =
+    "I individually developed the academic project Sistema de Pedidos, focusing on creating a web application to simulate the order flow. I was responsible for the entire structure and implementation of the system, which included designing the user interface, implementing the back-end logic, and ensuring smooth functionality. This project provided me with valuable experience in full-stack development and allowed me to showcase my ability to create a complete application from scratch.";
+
+  project3.innerText =
+    "I was responsible for the complete front-end of this academic project, taking care of building the interfaces and the user experience. I worked closely with the team to understand the requirements and translate them into a visually appealing and user-friendly design. This project allowed me to further develop my front-end skills and gain experience in creating responsive and interactive web applications.";
+
+  project4.innerText =
+    "I participated in the KON Support project working on both back-end and front-end, collaborating with the team in developing the system's functionalities. I was responsible for implementing features that enhanced the user experience and contributed to the overall functionality of the helpdesk system. This project provided me with valuable experience in full-stack development and allowed me to work in a collaborative environment, contributing to the success of the project.";
+
+  subtitleBackend.innerText = "Backend";
+  subtitleFrontend.innerText = "Frontend";
+  subtitleDatabase.innerText = "Database";
+  subtitleTools.innerText = "Tools";
+
+  headerAboutMe.innerText = "About me";
+  headerProjects.innerText = "Projects";
+  headerTools.innerText = "Technologies";
+  headerContact.innerText = "Contacts";
+
+  titleAboutMe.innerText = "About me";
+  titleProjects.innerText = "Projects";
+  titleTools.innerText = "Technologies";
+  titleContact.innerText = "Contacts";
+
+  phraseContact.innerText = "Let's work together!";
+  footerRights.innerText = "All rights reserved! " + "\u00A9" + " Marcelo";
+}
+
+function traduzirPt() {
+  aboutme = document.getElementById("about-me-text");
+  homeText = document.getElementById("home-text");
+  homeName = document.getElementById("home-name");
+
+  project1 = document.getElementById("project-1");
+  project2 = document.getElementById("project-2");
+  project3 = document.getElementById("project-3");
+  project4 = document.getElementById("project-4");
+
+  headerAboutMe = document.getElementById("header-about-me");
+  headerProjects = document.getElementById("header-projects");
+  headerTools = document.getElementById("header-tools");
+  headerContact = document.getElementById("header-contact");
+
+  titleAboutMe = document.getElementById("title-about-me");
+  titleProjects = document.getElementById("title-projects");
+  titleTools = document.getElementById("title-tools");
+  titleContact = document.getElementById("title-contacts");
+
+  subtitleBackend = document.getElementById("subtitle-backend");
+  subtitleFrontend = document.getElementById("subtitle-frontend");
+  subtitleDatabase = document.getElementById("subtitle-database");
+  subtitleTools = document.getElementById("subtitle-tools");
+
+  phraseContact = document.getElementById("phrase-contact");
+  footerRights = document.getElementById("footer-rights");
+
+  aboutme.innerText =
+    "Sou estudante de Análise e Desenvolvimento de Sistemas pela Unisul, com grande paixão por desenvolvimento de software e constante interesse em evoluir tecnicamente. Ao longo da minha formação, venho adquirindo sólidos conhecimentos em Java, Spring Boot, SQL e Angular, sempre aplicando boas práticas de programação, organização de código e foco em qualidade. Tenho perfil proativo, organizado e facilidade para aprender novas tecnologias, além de gostar de trabalhar em equipe e colaborar na resolução de problemas. Busco compreender não apenas a implementação, mas também a lógica de negócio e a arquitetura por trás das aplicações, com atenção especial ao desenvolvimento back-end. Possuo experiência em desenvolvimento full stack, utilizando Java com Spring Boot, Angular, PostgreSQL e versionamento com Git. Participo ativamente de projetos acadêmicos e estudo diariamente para me especializar cada vez mais em back-end, criando soluções eficientes, seguras e escaláveis.";
+
+  homeText.innerText =
+    "Bem-vindo ao meu portfólio! Aqui compartilho projetos e soluções com foco em desenvolvimento Fullstack. Fique à vontade para explorar meu trabalho e conhecer minha trajetória.";
+
+  homeName.innerText = "Sou o Marcelo";
+
+  project1.innerText =
+    "Participei como desenvolvedor front-end no projeto acadêmico Loja E-commerce Vivalis, minha primeira experiência prática no desenvolvimento de uma plataforma de e-commerce. Fui responsável por implementar a interface do usuário e garantir uma experiência fluida para os usuários. Este projeto me permitiu aplicar meus conhecimentos em tecnologias front-end e colaborar efetivamente com a equipe para entregar uma loja online funcional e visualmente atraente.";
+
+  project2.innerText =
+    "Desenvolvi individualmente o projeto acadêmico Sistema de Pedidos, com foco na criação de uma aplicação web para simular o fluxo de pedidos. Fui responsável por toda a estrutura e implementação do sistema, que incluiu o design da interface do usuário, a implementação da lógica de back-end e a garantia de uma funcionalidade suave. Este projeto me proporcionou uma valiosa experiência em desenvolvimento full-stack e me permitiu mostrar minha capacidade de criar uma aplicação completa do zero.";
+
+  project3.innerText =
+    "Atuei como responsável pelo front-end completo desse projeto acadêmico, cuidando da construção das interfaces e da experiência do usuário. Trabalhei em estreita colaboração com a equipe para entender os requisitos e traduzi-los em um design visualmente atraente e fácil de usar. Este projeto me permitiu desenvolver ainda mais minhas habilidades em front-end e ganhar experiência na criação de aplicações web responsivas e interativas.";
+
+  project4.innerText =
+    "Participei do projeto KON Support atuando em back-end e front-end, colaborando com a equipe no desenvolvimento das funcionalidades do sistema. Fui responsável pela implementação de recursos que melhoraram a experiência do usuário e contribuíram para a funcionalidade geral do sistema de helpdesk. Este projeto me proporcionou uma valiosa experiência em desenvolvimento full-stack e me permitiu trabalhar em um ambiente colaborativo, contribuindo para o sucesso do projeto.";
+
+  subtitleBackend.innerText = "Backend";
+  subtitleFrontend.innerText = "Frontend";
+  subtitleDatabase.innerText = "Banco de dados";
+  subtitleTools.innerText = "Ferramentas";
+
+  headerAboutMe.innerText = "Sobre mim";
+  headerProjects.innerText = "Projetos";
+  headerTools.innerText = "Tecnologias";
+  headerContact.innerText = "Contatos";
+
+  titleAboutMe.innerText = "Sobre mim";
+  titleProjects.innerText = "Projetos";
+  titleTools.innerText = "Tecnologias";
+  titleContact.innerText = "Contatos";
+
+  phraseContact.innerText = "Vamos trabalhar juntos!";
+  footerRights.innerText = "Direitos reservados! " + "\u00A9" + " Marcelo";
+}
